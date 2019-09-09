1171982c40e85d2bb8e48adb70686437743c7127
Hengky Wijaya <hengky.wijaya@tokopedia.com>
2019-09-09 13:04:46 +0700

    fix docker-compose error caused by container name

--- docker-compose/postgres/migration/users.sql
+++ docker-compose/postgres/migration/users.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE users(id serial  PRIMARY KEY,
+
+CREATE TABLE users (id serial  PRIMARY KEY,
 name text  NOT NULL,
-email text  NOT NULL,
+email text  NOT NULL
+);
+
 INSERT INTO users(name,email) VALUES('Ini Nama','hello@kid.con');
